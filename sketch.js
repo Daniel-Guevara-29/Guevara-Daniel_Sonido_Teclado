9dc15fa4ff37ea012d6fc222d2e708dffbc97bf2
daniel <daniel.guevara@ucuenca.edu.ec>
2023-10-30 15:37:51 -0500

    se agrego sonido burbuja

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+let sonidoBurbuja;
+
+function preload () {
+  soundFormats ('wav', 'mp3', 'ogg');
+  sonidoBurbuja = loadSound( 'sonidos/burbuja');
+}
+
+
+
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  let cnv = createCanvas(windowWidth, windowHeight);
+  cnv.mouseClicked(presionado);
+
+}
+
+function presionado (){
+  sonidoBurbuja.play();
+  circle(mouseX, mouseY, 20);
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  
+}
+
+function mouseDragged() {
+  sonidoBurbuja.play();
+  circle(mouseX, mouseY, 20) 
 }
